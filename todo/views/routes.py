492b3549dd504ab507ade2217cd850c911a47b5b
Aksh Garg <akshkgarg@gmail.com>
2025-02-26 11:31:44 +1000

    Second commit

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -1,54 +1,61 @@
 from flask import Blueprint, jsonify, request
+from datetime import datetime
 
 api = Blueprint('api',__name__, url_prefix='/api/v1')
 
-@api.route('/<username>')
-def login(username):
-    return f"Hey {username}, This is the login page."
-
-@api.route('/hello')
-def hello():
-    return jsonify({"status":"Hello Sir JI"})
-
-
 tasks = [] #Created a list to store TODO Tasks
 
+#Health API
+@api.route('/health')
+def heal():
+    return jsonify({"status":"ok"}), 200
+
+
+
 # get all tasks
-@api.route('/getData')
-def get_title():
-    return jsonify(tasks)
+@api.route('/todos', methods=['GET'])
+def get_tasks():
+    return jsonify(tasks),200
+
+
 
 # get by id
-@api.route('/getDataByID/<int:id>', methods=['GET'])
+@api.route('/todos/<int:id>', methods=['GET'])
 def get_title_by_id(id):
     for task in tasks:
         if task["id"]==id:
             return jsonify(task)
     return jsonify({"error":"the ID does not exists"}),400
 
+
+
 # post data
-@api.route('/addData', methods=['POST'])
+@api.route('/todos', methods=['POST'])
 def add_data():
     data = request.get_json()
     for task in tasks:
         if task["id"] == data["id"]:
             return jsonify({"error":"Task with same ID exists"}),400
     tasks.append(data)
-    return jsonify({"Success":"It's added"})
+    return jsonify({"Success":"It's added"}),200
+
+
 
 # update by id
-@api.route('updateData/<int:id>', methods=['PUT'])
+@api.route('todos/<int:id>', methods=['PUT'])
 def update_data(id):
     data = request.get_json()
     for task in tasks:
         if task["id"] == id:
             task.update(data)
             return jsonify({"Success":"Task updated", "Task":task}),200
+    else:
+        return jsonify({"error":"the id dosen't matches"}),400    
         
-        
+
         
 # delete by id
-@api.route('deleteByID/<int:id>', methods=['DELETE'])
+@api.route('todos/<int:id>', methods=['DELETE'])
 def delete_data(id):
     for task in tasks:
         if id == task["id"]:
